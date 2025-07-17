8d3eed055e4a40c679e75b059a7dd3f737122721
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2025-07-17 21:45:58 +0200

    css(layout): define basic palette

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,7 @@ const nextConfig: NextConfig = {
           options: {
             lessOptions: {
               javascriptEnabled: true,
+              modifyVars: {},
             },
           },
         },

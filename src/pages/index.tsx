a1f80916a6aeb1f7c1f680787da9e1dea641f196
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2025-07-24 16:21:59 +0200

    chore: change metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Rehabilitation module</title>
+        <title>Vitala</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className={`${geistSans.variable} ${geistMono.variable}`}>

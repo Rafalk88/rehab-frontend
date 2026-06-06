156d1177becb973b6d3e80a1bbff9442c761aae5
Rafalk88 <rafal.kochanecki@gmail.com>
2026-06-06 16:44:50 +0200

    test(TopNav): add integration test

--- src/components/layout/TopNav/TopNav.int-test.tsx
+++ src/components/layout/TopNav/TopNav.int-test.tsx
@@ -1,7 +1,35 @@
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
+jest.mock("@/hooks/useAutoLogout", () => ({
+  useAutoLogout: () => 15,
+}));
+
+jest.mock("@/components/UserMenu", () => ({
+  UserMenu: () => <div>UserMenu</div>,
+}));
+
+jest.mock("@/components/UnitSelector", () => ({
+  UnitSelector: () => <div>UnitSelector</div>,
+}));
+
+jest.mock("@/components/SearchBar", () => ({
+  SearchBar: () => <div>SearchBar</div>,
+}));
+
+jest.mock("./ApiConnectionIcon", () => ({
+  ApiConnectionIcon: () => <div>ApiConnectionIcon</div>,
+}));
+
+import { TopNav } from "./TopNav";
+
 describe("TopNav - integration test", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
+  it("should render all children", () => {
+    render(<TopNav />);
+
+    expect(screen.getByText("UserMenu")).toBeInTheDocument();
+    expect(screen.getByText("UnitSelector")).toBeInTheDocument();
+    expect(screen.getByText("SearchBar")).toBeInTheDocument();
+    expect(screen.getByText("ApiConnectionIcon")).toBeInTheDocument();
   });
 });

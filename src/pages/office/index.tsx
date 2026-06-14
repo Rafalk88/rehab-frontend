a5e5aade42af0804b68d896ec26114d0f0368f36
Rafalk88 <rafal.kochanecki@gmail.com>
2026-06-14 15:12:25 +0200

    feat(office): add basic table with data from db

--- src/pages/office/index.tsx
+++ src/pages/office/index.tsx
@@ -1,6 +1,7 @@
 import { useUser } from "@/store/useUser";
-import { useVisits } from "@/hooks/api/useVisits";
+import { useVisits, type Visit } from "@/hooks/api/useVisits";
 import { AppLayout } from "@/components/layout";
+import { Table } from "antd";
 
 export default function Office() {
   const userStore = useUser();
@@ -9,13 +10,36 @@ export default function Office() {
   const { data: response } = useVisits(orgUnit);
   const visits = response?.data;
 
+  const columns = [
+    {
+      title: "Lp.",
+      dataIndex: "lp",
+      key: "lp",
+      render: (_v: string, _r: Visit, i: number) => <p>{i + 1}</p>,
+    },
+    {
+      title: "Imię",
+      dataIndex: "name",
+      key: "name",
+      render: (_v: string, r: Visit) => <p>{r.patient.firstName?.firstName}</p>,
+    },
+    {
+      title: "Nazwisko",
+      dataIndex: "surname",
+      key: "surname",
+      render: (_v: string, r: Visit) => <p>{r.patient.surname?.surname}</p>,
+    },
+    {
+      title: "PESEL",
+      dataIndex: "pesel",
+      key: "pesel",
+      render: (_v: string, r: Visit) => <p>{r.patient.pesel}</p>,
+    },
+  ];
+
   return (
     <AppLayout>
-      <section>
-        {visits?.map(() => (
-          <div>1</div>
-        ))}
-      </section>
+      <Table dataSource={visits} columns={columns} rowKey="id" />
     </AppLayout>
   );
 }
